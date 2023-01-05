c40970c6cf09ce213efba44e24c4cfd9a0f4b1a9
Liliana Martins <lilianasrmartins@gmail.com>
2023-01-05 11:46:36 +0000

    added all components to app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,19 @@
+import React from "react";
+import "./App.css";
+import BusinessList from "../BusinessList/BusinessList.js";
+import SearchBar from "../SearchBar/SearchBar.js";
+import { render } from "@testing-library/react";
 
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        {< BusinessList />}
+        {< SearchBar/>}
+      </div>
+    );
+  }
 }
 
 export default App;
