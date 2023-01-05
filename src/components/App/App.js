b7cb66f8048ddbf74635a296fbd969fc3bbe43ce
Liliana Martins <lilianasrmartins@gmail.com>
2023-01-05 12:00:09 +0000

    tidy up

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "./App.css";
+
 import BusinessList from "../BusinessList/BusinessList.js";
 import SearchBar from "../SearchBar/SearchBar.js";
-import { render } from "@testing-library/react";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
-        {< BusinessList />}
-        {< SearchBar/>}
+        <SearchBar />
+        <BusinessList />
       </div>
     );
   }
--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -17,22 +17,20 @@ class Business extends React.Component {
   render() {
     return (
       <div className="Business">
-        <div class="image-container">
-          <img src={business.imageSrc} alt="" />
+        <div className="image-container">
+          <img src={business.imageSrc} alt=''/>
         </div>
-        <h2>MarginOtto Pizzeria</h2>
+        <h2>{business.name}</h2>
         <div className="Business-information">
           <div className="Business-address">
             <p>{business.address}</p>
             <p>{business.city}</p>
-            <p>
-              {business.state} {business.zipCode}
-            </p>
+            <p>{`${business.state} ${business.zipCode}`}</p>
           </div>
           <div className="Business-reviews">
-            <h3>ITALIAN</h3>
-            <h3 className="rating">4.5 stars&rbrace;</h3>
-            <p>90 reviews</p>
+            <h3>{business.category.toUpperCase()}</h3>
+            <h3 className="rating">{`${business.rating} stars`}</h3>
+            <p>{`${business.reviewCount} reviews`}</p>
           </div>
         </div>
       </div>
